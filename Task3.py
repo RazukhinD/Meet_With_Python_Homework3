4b17a34312dcef4c556b51e6a8869b4c2b10a854
Dmitrii <razuhindm@gmail.com>
2022-11-29 21:06:14 +0300

    Четвертая домашняя задача

--- Task3.py
+++ Task3.py
@@ -1,7 +1,7 @@
 number=int(input('Введи число которое надо преобразовать в двоичное:'))
 new_number=''
 while number>0:
-    new_number=str(number%2)+new_number
+    new_number = str(number%2)+new_number
     number=number//2
 print(new_number)
 
